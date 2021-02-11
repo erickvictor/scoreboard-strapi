52c847e70c9f5dfcd5384b047fd6c7479766f2fa
Erick Bernardo <erickvictor@gmail.com>
2021-02-11 19:18:30 -0400

    Add vercel

--- config/database.js
+++ config/database.js
@@ -1,14 +1,28 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "mongoose",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        uri: env("DATABASE_URI"),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
